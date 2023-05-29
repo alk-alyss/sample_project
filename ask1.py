01ba3367787074dffa924dbfd4b17e0233f353c1
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-29 11:56:04 +0300

    add main method

--- ask1.py
+++ ask1.py
@@ -1,2 +1,8 @@
 def subtract(a, b):
     return "POSITIVE" if a - b > 0 else "NEGATIVE"
+
+if __name__ == "__main__":
+	result1 = subtract(10, 5)
+	result2 = subtract(5, 10)
+	print(result1)
+	print(result2)
