9878582b6112a1a54661b71e6f43c5e8ccc28c56
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-29 12:27:53 +0300

    add unit tests

--- ask1.py
+++ ask1.py
@@ -1,7 +1,16 @@
 def subtract(a, b):
     return "POSITIVE" if a - b > 0 else "NEGATIVE"
 
+import unittest
+
+class SubtractTest(unittest.TestCase):
+	def test_subtract(self):
+		self.assertEqual(subtract(10, 5), "POSITIVE")
+		self.assertEqual(subtract(5, 10), "NEGATIVE")
+
 if __name__ == "__main__":
+	unittest.main()
+
 	result1 = subtract(10, 5)
 	result2 = subtract(5, 10)
 	print(result1)
